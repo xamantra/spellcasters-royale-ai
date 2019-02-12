351d7dc7e6e0f687c127f9a10da4b2715b740063
xamantra <xamantrx@gmail.com>
2019-02-12 23:03:36 +0800

    renamed SelectDirectionRandom to SelectRandomDirection

--- Assets/Scripts/Core/Direction.cs
+++ Assets/Scripts/Core/Direction.cs
@@ -2,7 +2,7 @@
 
 public static class Direction
 {
-    public static void SelectDirectionRandom(ref int desiredRotationY, ref Transform directionEngine, ref bool rotated)
+    public static void SelectRandomDirection(ref int desiredRotationY, ref Transform directionEngine, ref bool rotated)
     {
         desiredRotationY = Mathf.RoundToInt(Random.Range(0, 360));
         directionEngine.transform.rotation = Quaternion.Euler(0, desiredRotationY, 0);
