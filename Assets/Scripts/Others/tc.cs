55e7de6bdd70cbd6c7ec0930037063661511f93a
xamantra <xamantrx@gmail.com>
2019-02-12 18:14:08 +0800

    added try catch helper

---
+++ Assets/Scripts/Others/tc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class tc
+{
+    public static void Run(Action action)
+    {
+        try
+        {
+            action?.Invoke();
+        }
+        catch
+        {
+            return;
+        }
+    }
+}
