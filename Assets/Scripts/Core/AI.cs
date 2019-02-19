7311fcd5d706c294d0aa5cc0efb5142a1e78a75f
xamantra <xamantrx@gmail.com>
2019-02-20 06:49:40 +0800

    Revert "messed-up codes."
    This reverts commit 194e33e240f38f1fc6df7352891b179d3fcca014.

--- Assets/Scripts/Core/AI.cs
+++ Assets/Scripts/Core/AI.cs
@@ -16,8 +16,6 @@ public class AI : MonoBehaviour
     [SerializeField] private LayerMask lootLayerMask;
     [SerializeField] private Transform directionEngine;
     [SerializeField] private Transform directionGuide;
-    [SerializeField] private Eyes eyes;
-    [SerializeField] private Transform visionRange;
     [SerializeField] private float directionGuideElevation;
 
     [Header("Debug Colors")]
@@ -40,7 +38,6 @@ public class AI : MonoBehaviour
     private bool enemyDetected;
     private bool lootInRange;
     private bool enemyInRange;
-    private bool hasVision;
     private Vector3 currentDestination;
     private float remainingDistance;
     private bool gettingNearestObject;
@@ -50,14 +47,6 @@ public class AI : MonoBehaviour
     private float attackIntervalTimer;
     private Collider[] scannedWeapons;
     private Collider[] scannedEnemies;
-    #endregion
-
-    #region properties
-    public Eyes Eyes { get { return eyes; } }
-    public float AttackRange { get { return attackRange; } }
-    public LayerMask EnemyLayerMask { get { return enemyLayerMask; } }
-    public bool DirectionSelected { get { return directionSelected; } }
-    public bool HasVision { get { return hasVision; } }
     #endregion
     #endregion
 
@@ -247,8 +236,6 @@ public class AI : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position, attackRange);
         Gizmos.color = Color.cyan;
         Gizmos.DrawWireSphere(transform.position, sphereCastRadius);
-        Gizmos.color = hasVision ? Color.red : Color.cyan;
-        Gizmos.DrawLine(transform.position + new Vector3(0, visionRange.localPosition.y, 0), visionRange.position);
     }
     #endregion
 
@@ -257,8 +244,6 @@ public class AI : MonoBehaviour
     {
         if (directionGuide != null)
             directionGuide.localPosition = new Vector3(0, directionGuideElevation, roamRange);
-        if (visionRange != null)
-            visionRange.localPosition = new Vector3(0, visionRange.localPosition.y, attackRange);
         player = GetComponent<Player>();
         agent = GetComponent<NavMeshAgent>();
         collider = GetComponent<Collider>();
@@ -268,17 +253,15 @@ public class AI : MonoBehaviour
     #region AI methods
     private void UpdateStates()
     {
-        eyes.Open(attackRange, visionRange.position, enemyLayerMask, transform.position + new Vector3(0, visionRange.localPosition.y, 0));
         lootDetected = Sensor.InRange<IWeapon>(transform, collider, roamRange, lootLayerMask);
-        enemyDetected = Sensor.InRange<Player>(transform, collider, roamRange, enemyLayerMask);
-        //enemyDetected = Sensor.InSight<Player>(transform, sphereCastRadius, roamRange, enemyLayerMask);
+        //enemyDetected = Sensor.InRange<Player>(transform, collider, roamRange, enemyLayerMask);
+        enemyDetected = Sensor.InSight<Player>(transform, sphereCastRadius, roamRange, enemyLayerMask);
         lootInRange = Sensor.InRange<IWeapon>(transform, collider, pickupRange, lootLayerMask);
-        enemyInRange = Sensor.InRange<Player>(transform, collider, attackRange, enemyLayerMask);
-        //enemyInRange = Sensor.InSight<Player>(transform, sphereCastRadius, attackRange, enemyLayerMask);
-        hasVision = eyes.HasVision();
+        //enemyInRange = Sensor.InRange<Player>(transform, collider, attackRange, enemyLayerMask);
+        enemyInRange = Sensor.InSight<Player>(transform, sphereCastRadius, attackRange, enemyLayerMask);
     }
 
-    public void FindDirection()
+    private void FindDirection()
     {
         Direction.SelectRandomDirection(ref desiredRotationY, ref directionEngine, ref directionSelected);
     }
@@ -290,7 +273,7 @@ public class AI : MonoBehaviour
 
     private void Attack()
     {
-        Actions.Attack(this, ref nearestEnemy, ref player, ref attackIntervalTimer, transform);
+        Actions.Attack(ref nearestEnemy, ref player, ref attackIntervalTimer, transform);
     }
 
     private void Move()
--- Assets/Scripts/Core/Actions.cs
+++ Assets/Scripts/Core/Actions.cs
@@ -9,20 +9,13 @@ public static class Actions
         agent.SetDestination(transform.position);
     }
 
-    public static void Attack(AI ai, ref Player enemy, ref Player self, ref float attackIntervalTimer, Transform transform)
+    public static void Attack(ref Player enemy, ref Player self, ref float attackIntervalTimer, Transform transform)
     {
         var canAttack = enemy != null && self != null && self.CanAttack();
         if (canAttack && attackIntervalTimer <= 0)
         {
             transform.LookAt(enemy.transform);
-            if (ai.HasVision)
-            {
-                self.Attack();
-            }
-            else if (!ai.DirectionSelected)
-            {
-                ai.FindDirection();
-            }
+            self.Attack();
         }
     }
 }
